94d0e1b2452cadfa456eea449e8a174d476ec10a
Mordosaurus <kalidcraft@gmail.com>
2023-05-26 22:51:52 -0600

    Ya medio funciona version 1

--- phaser-webpack-boilerplate/src/index.js
+++ phaser-webpack-boilerplate/src/index.js
@@ -1,21 +1,22 @@
-import Phaser, { Scenes } from "phaser";
+import Phaser, {scenes} from "phaser";
 import GameScene from "./scenes/main-scene";
 
 const GLOBAL_CONFIG = {
-  width: 800,
-  height: 600,
-}
-
-const config = {
-  type: Phaser.AUTO,
-  ...GLOBAL_CONFIG,
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: true,
-      gravity: {y: 400}
-    }
-  },
-  scene: [new GameScene(GLOBAL_CONFIG)]
-}
-new Phaser.Game(config);
+    width: 800,
+    height: 600,
+  }
+  
+  const config = {
+    type: Phaser.AUTO,
+    ...GLOBAL_CONFIG,
+    pixelArt: true,
+    physics: {
+      default: "arcade",
+      arcade: {
+        debug: true,
+        gravity: {y: 0}
+      }
+    },
+    scene: [new GameScene(GLOBAL_CONFIG)]
+  }
+  new Phaser.Game(config);
--- phaser-webpack-boilerplate/src/scenes/main-scene.js
+++ phaser-webpack-boilerplate/src/scenes/main-scene.js
@@ -1,105 +1,256 @@
 export default class GameScene extends Phaser.Scene {
   constructor(config) {
-    console.log("This is the main game scene");
     super(config);
     this.config = config;
     this.tank = null;
     this.projectiles = null;
+    this.enemyProjectiles = null;
+    this.enemies = null;
+    this.enemyMovementTimer = null;
+    this.tankSpeed = 200;
+    this.enemySpeed = 20;
+    this.enemyDelay = 1000;
+    this.enemyPauseDuration = 500;
+    this.enemyDirection = 1;
+    this.enemyVerticalShift = 20;
+    this.enemyHorizontalShift = 10;
+    this.enemyProjectileSpeed = 300;
+    this.gameOverTimer = null;
+    this.gameOverText = null;
+    this.restartCountdownTimer = null;
+    this.restartCountdownText = null;
+    this.restartDelay = 5000;
   }
 
   preload() {
-    //Carga assets
     this.load.image("sky", "assets/SpaceBackground.jpg");
     this.load.image("tank", "assets/Tank.png");
     this.load.image("projectile", "assets/Projectile.png");
+    this.load.image("enemy", "assets/Enemy.png");
+    this.load.image("enemyProjectile", "assets/EnemyProjectile.png");
+    this.load.image("pauseButton", "assets/pause.png");
   }
 
   create() {
-    //Cambia el pivote del cielo
     this.add.image(0, 0, "sky").setOrigin(0);
 
-    //Tank
-    this.tank = this.add.sprite(100, this.config.height - 50, "tank");
+    this.tank = this.physics.add.sprite(this.config.width / 2, this.config.height - 50, "tank");
     this.tank.setScale(0.1);
-    this.physics.add.existing(this.tank);
-
-    //Configuración de movimiento
-    this.tank.body.velocity.x = 0;
     this.tank.body.allowGravity = false;
+    this.tank.setCollideWorldBounds(true);
 
-    //Configuración de colisiones
-    this.tank.body.setCollideWorldBounds(true);
+    this.projectiles = this.physics.add.group();
+    this.enemyProjectiles = this.physics.add.group();
+    this.enemies = this.physics.add.group();
+    this.createEnemies();
+    this.createKeyboardControls();
+    this.startEnemyMovement();
+    this.startEnemyShooting();
 
-    //Configuración de controles
+    this.physics.add.collider(this.projectiles, this.enemies, this.projectileHitEnemy, null, this);
+    this.physics.add.collider(this.enemyProjectiles, this.tank, this.projectileHitTank, null, this);
+    this.physics.add.collider(this.enemies, this.tank, this.enemyHitTank, null, this);
+
+    const pauseButton = this.add.image(this.config.width - 50, 50, "pauseButton");
+    pauseButton.setInteractive();
+    pauseButton.on("pointerdown", () => {
+      this.scene.pause();
+    });
+  }
+
+  createEnemies() {
+    const enemyX = 100;
+    const enemyY = 100;
+    const enemySpacing = 80;
+
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 4; j++) {
+        const enemy = this.physics.add.sprite(enemyX + i * enemySpacing, enemyY + j * enemySpacing, "enemy");
+        enemy.setScale(0.05);
+        enemy.body.allowGravity = false;
+        enemy.setCollideWorldBounds(true);
+        this.enemies.add(enemy);
+      }
+    }
+  }
+
+  createKeyboardControls() {
     const cursors = this.input.keyboard.createCursorKeys();
-    const speed = 150;
 
     cursors.left.on("down", () => {
-      this.tank.body.velocity.x = -speed;
+      this.moveTankLeft();
     });
 
     cursors.right.on("down", () => {
-      this.tank.body.velocity.x = speed;
+      this.moveTankRight();
     });
 
     cursors.left.on("up", () => {
-      this.tank.body.velocity.x = 0;
+      if (!cursors.right.isDown) {
+        this.stopTank();
+      }
     });
 
     cursors.right.on("up", () => {
-      this.tank.body.velocity.x = 0;
+      if (!cursors.left.isDown) {
+        this.stopTank();
+      }
     });
 
-    // Creación del grupo de proyectiles
-    this.projectiles = this.physics.add.group({
-      maxSize: 10,
-      classType: Projectile,
-    });
-
-    //Configuración de disparo
-    const spacebar = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.SPACE
-    );
+    const spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     spacebar.on("down", () => {
-      const projectile = this.projectiles.get();
-      projectile.fire(this.tank.x, this.tank.y);
+      this.fireProjectile();
     });
   }
 
-  update() {
-    // Ocultar proyectiles al salir de la pantalla
-    this.projectiles.getChildren().forEach((projectile) => {
-      if (projectile.y < 0) {
-        projectile.setVisible(false);
+  moveTankLeft() {
+    this.tank.body.velocity.x = -this.tankSpeed;
+  }
+
+  moveTankRight() {
+    this.tank.body.velocity.x = this.tankSpeed;
+  }
+
+  stopTank() {
+    this.tank.body.velocity.x = 0;
+  }
+
+  fireProjectile() {
+    const projectile = this.physics.add.sprite(this.tank.x, this.tank.y - 20, "projectile");
+    projectile.setScale(0.1);
+    this.projectiles.add(projectile);
+    projectile.body.velocity.y = -500;
+    const randomEnemy = Phaser.Utils.Array.GetRandom(this.enemies.getChildren());
+  const enemyProjectile = this.physics.add.sprite(randomEnemy.x, randomEnemy.y + 20, "enemyProjectile");
+  enemyProjectile.setScale(0.1);
+  this.enemyProjectiles.add(enemyProjectile); // Add enemy projectile to the 'enemyProjectiles' group
+
+    projectile.body.onWorldBounds = true;
+    this.physics.world.on("worldbounds", (body) => {
+      if (body.gameObject === projectile && body.up) {
+        projectile.destroy();
       }
     });
   }
-}
 
-class Projectile extends Phaser.GameObjects.Image {
-  constructor(scene, x, y) {
-    super(scene, x, y, "projectile");
-    this.speed = Phaser.Math.GetSpeed(600, 1);
+  startEnemyMovement() {
+    this.enemyMovementTimer = this.time.addEvent({
+      delay: this.enemyDelay,
+      loop: true,
+      callback: () => {
+        this.moveEnemies();
+      },
+    });
   }
 
+  moveEnemies() {
+    const enemyChildren = this.enemies.getChildren();
+    let reachedRightEdge = false;
+    let reachedLeftEdge = false;
+    let reachedBottomEdge = false;
 
-  fire(x, y) {
-    this.setVisible(true);
-    this.setActive(true);
-    this.setPosition(x, y - 20);
-    this.body.velocity.y = -1500; // Velocidad Del Proyectil
-  }
-
-  update(time, delta) {
-    this.y -= this.speed * delta;
-    
-
-    
-    // Ocultar proyectil al salir de la pantalla
-    if (this.y < 0) {
-      this.setVisible(false);
-      this.setActive(false);
+    for (let i = 0; i < enemyChildren.length; i++) {
+      const enemy = enemyChildren[i];
+      enemy.x += this.enemySpeed * this.enemyDirection;
     }
+
+    for (let i = 0; i < enemyChildren.length; i++) {
+      const enemy = enemyChildren[i];
+      if (enemy.body.right >= this.config.width - this.enemyHorizontalShift) {
+        reachedRightEdge = true;
+      } else if (enemy.body.left <= this.enemyHorizontalShift) {
+        reachedLeftEdge = true;
+      }
+
+      if (enemy.body.bottom >= this.config.height - this.enemyVerticalShift) {
+        reachedBottomEdge = true;
+      }
+    }
+
+    if (reachedRightEdge) {
+      this.enemyDirection = -1;
+      for (let i = 0; i < enemyChildren.length; i++) {
+        const enemy = enemyChildren[i];
+        enemy.y += this.enemyVerticalShift;
+      }
+    } else if (reachedLeftEdge) {
+      this.enemyDirection = 1;
+      for (let i = 0; i < enemyChildren.length; i++) {
+        const enemy = enemyChildren[i];
+        enemy.y += this.enemyVerticalShift;
+      }
+    }
+
+    if (reachedBottomEdge) {
+      this.enemyHitTank();
+    }
+  }
+
+  startEnemyShooting() {
+    this.time.addEvent({
+      delay: Phaser.Math.Between(2000, 5000),
+      loop: true,
+      callback: () => {
+        this.enemyFireProjectile();
+      },
+    });
+  }
+
+  enemyFireProjectile() {
+    const randomEnemy = Phaser.Utils.Array.GetRandom(this.enemies.getChildren());
+    const enemyProjectile = this.physics.add.sprite(randomEnemy.x, randomEnemy.y + 20, "enemyProjectile");
+    enemyProjectile.setScale(0.1);
+    this.enemyProjectiles.add(enemyProjectile);
+    enemyProjectile.body.velocity.y = this.enemyProjectileSpeed;
+  
+    this.physics.add.collider(enemyProjectile, this.tank, this.projectileHitTank, null, this);
+  }
+
+  projectileHitTank(projectile, tank) {
+    projectile.destroy(); // Destroy the projectile
+    tank.destroy(); // Destroy the tank/player
+    this.gameOver(); // Trigger game over condition
+  }
+
+  projectileHitEnemy(projectile, enemy) {
+    projectile.destroy();
+    enemy.destroy();
+  }
+
+  enemyHitTank() {
+    this.gameOver();
+  }
+
+  gameOver() {
+    this.scene.pause();
+    this.gameOverText = this.add.text(this.config.width / 2, this.config.height / 2, "¡Has perdido!", {
+      fontSize: "32px",
+      fill: "#fff",
+    }).setOrigin(0.5);
+    this.restartCountdownText = this.add.text(this.config.width / 2, this.config.height / 2 + 50, "", {
+      fontSize: "24px",
+      fill: "#fff",
+    }).setOrigin(0.5);
+
+    this.gameOverTimer = this.time.delayedCall(this.restartDelay, () => {
+      this.restartGame();
+    });
+
+    this.restartCountdownTimer = this.time.addEvent({
+      delay: 1000,
+      repeat: 4,
+      callback: () => {
+        const remainingTime = 4 - this.restartCountdownTimer.repeatCount;
+        this.restartCountdownText.setText(`El juego se reiniciará en ${remainingTime}...`);
+      },
+      onComplete: () => {
+        this.restartCountdownText.setText("");
+      },
+    });
+  }
+
+  restartGame() {
+    this.scene.restart();
   }
 }
